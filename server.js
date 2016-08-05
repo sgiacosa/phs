b36e5b68974317ce83f38a04c7bddbaae4e0ca69
sgiacosa <sebacipo@gmail.com>
2016-08-05 13:02:17 -0300

    2

--- server.js
+++ server.js
@@ -15,67 +15,17 @@ var http_ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 var app = express();
 
-var secret = 'hfy6*fre34#frtu!';
-
-console.log("1. Preparar conexión a la base de datos ...");
-//mongoose.connect('mongodb://localhost/raph');
-
-console.log("2. Configurando middleware ...")
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-
 console.log("3. Configurando rutas estáticas ...")
-//app.use('/', expressJwt({secret: secret}));
-//app.use('/', express.static('public'));
-//app.use('/', express.static(path.join(__dirname, '', 'public')));
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(path.join(__dirname, '', 'public')));
+app.listen(http_port, http_ip);
+console.log('Server running on http://%s:%s', http_ip, http_port);
 
-console.log("5. Configurando socket.io ...")
-/*var server = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, '', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '', 'cert.pem'))
-}, app).listen(https_port);*/
-
-var server = http.createServer(app).listen(http_port, http_ip);
-
-var io = require('socket.io').listen(server);
-socket(io);
-
-console.log("4. Configurando API ...")
-
-var auths = requireDir('./auth/');
-for (var a in auths) {
-  app.use('/auth/', auths[a]);
-}
-
-//Protejo las api, solo se accede con un token válido
-app.use('/api', expressJwt({ secret: secret }));
-
-var routes = requireDir('./api/');
-for (var route in routes) {
-  app.use('/api/', routes[route](io));
-}
-//Sino esta logeado devuelvo 401
-app.use(function (err, req, res, next) {
-  if (err.constructor.name === 'UnauthorizedError') {
-    res.status(401).send('Unauthorized');
-  }
-});
-
-app.get('/', function (req, res) {
-    res.status('200').send('Service is up');
-});
-
-// Para cualquier otra ruta, devuelve el archivo principal de la aplicación de Angular
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '', 'public/index.html'));
+app.get('/', function (req, res) {  
+    res.send('{ pageCount: -1 }');
 });
 
 
 
-console.log("Todo Listo!")
-
+module.exports = app ;
+//var server = http.createServer(app).listen(http_port, http_ip);
 //app.listen(3000);
